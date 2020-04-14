9d1e1b5d5758c2a9e70c41067d1b7130c3ac5314
yutotanigawa <61495631+yutotanigawa@users.noreply.github.com>
2020-04-15 01:37:37 +0900

    adminページのテスト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-	  devise_for :users
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+  }
+  devise_for :users
 
 
   root 'home#top'
